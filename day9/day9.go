1778760983a4476352f10c01732a3a47e51fe7db
Jonathan Blashki <jblashki@gmail.com>
2020-12-02 01:05:57 +1030

    Added day 9

--- day9/day9.go
+++ day9/day9.go
@@ -3,9 +3,14 @@ package day9
 import (
 	"errors"
 	"fmt"
+	"sync"
+
+	"github.com/jblashki/aoc-intcode-go/v3"
 )
 
 const name = "Day 9"
+
+const input_file = "./day9/program"
 
 func RunDay(verbose bool) {
 	var aResult int
@@ -16,14 +21,14 @@ func RunDay(verbose bool) {
 		fmt.Printf("\n%v Output:\n", name)
 	}
 
-	aResult, err = a()
+	aResult, err = a(verbose)
 	if err != nil {
 		fmt.Printf("%va: **** Error: %q ****\n", name, err)
 	} else {
 		fmt.Printf("%va: Program Result = %v\n", name, aResult)
 	}
 
-	bResult, err = b()
+	bResult, err = b(verbose)
 	if err != nil {
 		fmt.Printf("%vb: **** Error: %q ****\n", name, err)
 	} else {
@@ -31,10 +36,62 @@ func RunDay(verbose bool) {
 	}
 }
 
-func a() (int, error) {
-	return 0, errors.New("Not Complete Yet")
+func a(verbose bool) (int, error) {
+	value, err := boost(1, verbose)
+	return value, err
 }
 
-func b() (int, error) {
-	return 0, errors.New("Not Complete Yet")
+func b(verbose bool) (int, error) {
+	value, err := boost(2, verbose)
+	return value, err
+}
+
+func boost(input int, verbose bool) (int, error) {
+	ic := intcode.Create()
+
+	err := intcode.Load(ic, input_file)
+	if err != nil {
+		return 0, err
+	}
+
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+
+	inputChan := make(chan int, 1)
+	outputChan := make(chan int)
+	haltSignalChan := make(chan int, 1)
+	errorMsgChan := make(chan string, 1)
+
+	defer func() {
+		close(inputChan)
+		close(outputChan)
+		close(haltSignalChan)
+		close(errorMsgChan)
+	}()
+
+	inputChan <- input
+
+	go intcode.Run(ic, inputChan, outputChan, haltSignalChan, errorMsgChan, wg, "")
+
+	value := -1
+	exit := false
+	for !exit {
+		select {
+		case value = <-outputChan:
+			if verbose {
+				fmt.Printf("> %v\n", value)
+			}
+		case hltValue := <-haltSignalChan:
+			if hltValue == 1 {
+				err := <-errorMsgChan
+				errormsg := fmt.Sprintf("Program halted with error: %v", err)
+				return 0, errors.New(errormsg)
+			}
+			exit = true
+		}
+	}
+
+	wg.Wait()
+
+	return value, nil
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/jblashki/aoc-filereader-go v1.1.0
 	github.com/jblashki/aoc-intcode-go v1.1.0
 	github.com/jblashki/aoc-intcode-go/v2 v2.0.0
+	github.com/jblashki/aoc-intcode-go/v3 v3.0.0
 )
