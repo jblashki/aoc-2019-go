7d6f8aa89b9ad013c0043c8df36b13e2056c4ac2
Jonathan Blashki <jblashki@gmail.com>
2020-12-12 11:16:09 +1100

    Day 13

--- day13/day13.go
+++ day13/day13.go
@@ -1,8 +1,6 @@
 package day13
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -45,7 +43,7 @@ func RunDay(verbose bool) {
 		fmt.Printf("%va: Program Result = %v\n", name, aResult)
 	}
 
-	bResult, err = b()
+	bResult, err = b(verbose)
 	if err != nil {
 		fmt.Printf("%vb: **** Error: %q ****\n", name, err)
 	} else {
@@ -98,61 +96,32 @@ func printTile(screen [][]tile, t tile, xPos int, yPos int) [][]tile {
 	return screen
 }
 
-func pressEnter() {
-	reader := bufio.NewReader(os.Stdin)
-	//fmt.Print("<PRESS ENTER TO CONTINUE>")
-	reader.ReadString('\n')
+func findBallPaddleXPos(screen [][]tile) (ball int, paddle int) {
+	for _, line := range screen {
+		for xPos, tile := range line {
+			if tile == tileBall {
+				ball = xPos
+			} else if tile == tilePaddle {
+				paddle = xPos
+			}
+		}
+	}
 
+	return
 }
 
-// func getChar() (ascii int, keyCode int, err error) {
-// 	t, _ := term.Open("/dev/tty")
-// 	term.RawMode(t)
-// 	bytes := make([]byte, 3)
-
-// 	var numRead int
-// 	numRead, err = t.Read(bytes)
-// 	if err != nil {
-// 		return
-// 	}
-// 	if numRead == 3 && bytes[0] == 27 && bytes[1] == 91 {
-// 		// Three-character control sequence, beginning with "ESC-[".
-
-// 		// Since there are no ASCII codes for arrow keys, we use
-// 		// Javascript key codes.
-// 		if bytes[2] == 65 {
-// 			// Up
-// 			keyCode = 38
-// 		} else if bytes[2] == 66 {
-// 			// Down
-// 			keyCode = 40
-// 		} else if bytes[2] == 67 {
-// 			// Right
-// 			keyCode = 39
-// 		} else if bytes[2] == 68 {
-// 			// Left
-// 			keyCode = 37
-// 		}
-// 	} else if numRead == 1 {
-// 		ascii = int(bytes[0])
-// 	} else {
-// 		// Two characters read??
-// 	}
-// 	t.Restore()
-// 	t.Close()
-// 	return
-// }
-
-func a(verbose bool) (int, error) {
+func playGame(setQuarters bool, verbose bool) ([][]tile, int, error) {
 	var err error
 
 	wg := new(sync.WaitGroup)
 	ic, err := intcode.CreateLoad(wg, inputFile, 0, 0)
 	if err != nil {
-		return 0, err
+		return nil, 0, err
 	}
 
-	intcode.Set(ic, 0, 2)
+	if setQuarters {
+		intcode.Set(ic, 0, 2)
+	}
 
 	defer intcode.Close(ic)
 
@@ -161,15 +130,12 @@ func a(verbose bool) (int, error) {
 
 	screen := make([][]tile, 0)
 	score := 0
-	//exit := false
 	xPos := 0
 	yPos := 0
 	tileID := 0
-	update := 0
 	var sig intcode.Signal
 
 	for {
-		//control := 0
 		exitLoop := false
 		for !exitLoop {
 			xPos, sig, err = intcode.Read(ic)
@@ -177,37 +143,20 @@ func a(verbose bool) (int, error) {
 			case intcode.SigNone:
 				exitLoop = true
 			case intcode.SigHalt:
-				return 0, fmt.Errorf("HALT")
+				return screen, score, nil
 
 			case intcode.SigError:
-				return 0, fmt.Errorf("Program error: %v", err)
+				return screen, score, fmt.Errorf("Program error: %v", err)
 
 			case intcode.SigInput:
-				drawScreen(screen, score)
-				fmt.Printf("? ")
-				reader := bufio.NewReader(os.Stdin)
-				char, _, err := reader.ReadRune()
-				if err != nil {
-					return 0, err
-				}
+				ballPos, paddlePos := findBallPaddleXPos(screen)
 
-				switch char {
-				case 'a':
+				if ballPos < paddlePos {
 					intcode.Write(ic, -1)
-				case 'd':
+				} else if ballPos > paddlePos {
 					intcode.Write(ic, 1)
-				default:
+				} else {
 					intcode.Write(ic, 0)
-				}
-
-				switch sig {
-				// case intcode.SigNone:
-
-				case intcode.SigHalt:
-					return 0, fmt.Errorf("HALT")
-
-				case intcode.SigError:
-					return 0, fmt.Errorf("Program error: %v", err)
 				}
 			}
 		}
@@ -220,37 +169,20 @@ func a(verbose bool) (int, error) {
 				exitLoop = true
 
 			case intcode.SigHalt:
-				return 0, fmt.Errorf("HALT")
+				return screen, score, nil
 
 			case intcode.SigError:
-				return 0, fmt.Errorf("Program error: %v", err)
+				return screen, score, fmt.Errorf("Program error: %v", err)
 
 			case intcode.SigInput:
-				drawScreen(screen, score)
-				fmt.Printf("? ")
-				reader := bufio.NewReader(os.Stdin)
-				char, _, err := reader.ReadRune()
-				if err != nil {
-					return 0, err
-				}
+				ballPos, paddlePos := findBallPaddleXPos(screen)
 
-				switch char {
-				case 'a':
+				if ballPos < paddlePos {
 					intcode.Write(ic, -1)
-				case 'd':
+				} else if ballPos > paddlePos {
 					intcode.Write(ic, 1)
-				default:
+				} else {
 					intcode.Write(ic, 0)
-				}
-
-				switch sig {
-				// case intcode.SigNone:
-
-				case intcode.SigHalt:
-					return 0, fmt.Errorf("HALT")
-
-				case intcode.SigError:
-					return 0, fmt.Errorf("Program error: %v", err)
 				}
 			}
 		}
@@ -263,53 +195,42 @@ func a(verbose bool) (int, error) {
 				exitLoop = true
 
 			case intcode.SigHalt:
-				return 0, fmt.Errorf("HALT")
+				return screen, score, nil
 
 			case intcode.SigError:
-				return 0, fmt.Errorf("Program error: %v", err)
+				return screen, score, fmt.Errorf("Program error: %v", err)
 
 			case intcode.SigInput:
-				drawScreen(screen, score)
-				fmt.Printf("? ")
-				reader := bufio.NewReader(os.Stdin)
-				char, _, err := reader.ReadRune()
-				if err != nil {
-					return 0, err
-				}
+				ballPos, paddlePos := findBallPaddleXPos(screen)
 
-				switch char {
-				case 'a':
+				if ballPos < paddlePos {
 					intcode.Write(ic, -1)
-				case 'd':
+				} else if ballPos > paddlePos {
 					intcode.Write(ic, 1)
-				default:
+				} else {
 					intcode.Write(ic, 0)
-				}
-
-				switch sig {
-				// case intcode.SigNone:
-
-				case intcode.SigHalt:
-					return 0, fmt.Errorf("HALT")
-
-				case intcode.SigError:
-					return 0, fmt.Errorf("Program error: %v", err)
 				}
 			}
 		}
 
 		if xPos == -1 {
 			score = tileID
+			if verbose {
+				fmt.Printf("Score: %v\n", score)
+			}
 		} else {
 			t := tile(tileID)
 			screen = printTile(screen, t, xPos, yPos)
 		}
 
-		update++
+		// update++
 	}
+}
 
-	if verbose {
-		drawScreen(screen, score)
+func a(verbose bool) (int, error) {
+	screen, _, err := playGame(false, verbose)
+	if err != nil {
+		return 0, err
 	}
 
 	blockCount := 0
@@ -324,6 +245,24 @@ func a(verbose bool) (int, error) {
 	return blockCount, nil
 }
 
-func b() (int, error) {
-	return 0, errors.New("Not Complete Yet")
+func b(verbose bool) (int, error) {
+	screen, score, err := playGame(true, verbose)
+	if err != nil {
+		return 0, err
+	}
+
+	blockCount := 0
+	for _, line := range screen {
+		for _, tile := range line {
+			if tile == tileBlock {
+				blockCount++
+			}
+		}
+	}
+
+	if blockCount != 0 {
+		return 0, fmt.Errorf("Failed to finish game. %v blocks remaining", blockCount)
+	}
+
+	return score, nil
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module aoc2019
 
-replace github.com/jblashki/aoc-intcode-go/v5 => /home/jblashki/development/golang/advent/modules/intcode/v5
-
 go 1.14
 
 require (
@@ -9,7 +7,7 @@ require (
 	github.com/jblashki/aoc-intcode-go v1.1.0
 	github.com/jblashki/aoc-intcode-go/v2 v2.0.1
 	github.com/jblashki/aoc-intcode-go/v3 v3.0.1
-	github.com/jblashki/aoc-intcode-go/v4 v4.0.0
+	github.com/jblashki/aoc-intcode-go/v4 v4.0.1
 	github.com/jblashki/aoc-intcode-go/v5 v5.0.0
 	github.com/pkg/term v1.1.0
 )
